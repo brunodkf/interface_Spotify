bb70e6992f95a0d93cd2db0895b635edadcde88f
Bruno Furtado <bsouzacc@gmail.com>
2020-12-26 21:02:53 -0300

    Adicionando JS

--- js/spotify.js
+++ js/spotify.js
@@ -1,1 +1,38 @@
-let extra = document.querySelector('.perfil');
+let botao = document.querySelector('.login');
+let faz = true;
+
+botao.addEventListener('click', function(){
+
+     if(faz){
+          mostraSubMenu(); 
+          faz = false;
+
+     }else{
+
+         escondeSubmenu();
+         faz = true;
+     }
+  
+   
+});
+
+function mostraSubMenu(){
+
+     let submenu = document.querySelector('.submenu');  
+     submenu.classList.add('mostra-submenu');
+
+     let trocaBotao = document.querySelector('.botao-submenu');
+     trocaBotao.innerHTML = '<i style="font-size:24px" class="fa">&#xf106;</i>';
+
+}
+
+function escondeSubmenu(){
+   
+        
+    let submenu = document.querySelector('.submenu');
+    submenu.classList.remove('mostra-submenu');
+
+    let trocaBotao = document.querySelector('.botao-submenu');
+    trocaBotao.innerHTML = '<i style="font-size:24px" class="fa">&#xf107;</i>';
+
+}
